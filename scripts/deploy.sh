c46ca69cc61cac664c3e67fe9b4245f1349a7cdb
Anvith <anvithgowda2219@gmail.com>
2026-01-11 16:37:57 +0530

    Updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,12 @@
 IMAGE_NAME="anvith3068/python-devops-app:latest"
 CONTAINER_NAME="python-app"
 
-docker rm -f $CONTAINER_NAME || true
+echo "Stopping any container using port 5000..."
+
+docker ps -q --filter "publish=5000" | xargs -r docker rm -f
+
+echo "Starting new container..."
 
 docker run -d -p 5000:5000 --name $CONTAINER_NAME $IMAGE_NAME
+
 
